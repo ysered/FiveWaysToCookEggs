a6a7877b58c892074d2fa944d477ab525a548510
Yuriy Seredyuk <yuriy.seredyuk@gmail.com>
2016-03-24 19:54:46 +0200

    Use recipe data source.

--- app/src/main/java/com/example/android/fivewaystocookeggs/MainActivity.java
+++ app/src/main/java/com/example/android/fivewaystocookeggs/MainActivity.java
@@ -18,49 +18,10 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView recipesRecyclerView = (RecyclerView) findViewById(R.id.recipes_recycler_view);
         recipesRecyclerView.setHasFixedSize(true);
 
+        final Recipe[] recipes = new RecipeDataSource(this).getRecipes();
         final RecyclerView.Adapter adapter =
-                new RecipeCardsAdapter(this, MainActivity.this, getRecipes());
+                new RecipeCardsAdapter(this, MainActivity.this, recipes);
         recipesRecyclerView.setAdapter(adapter);
-    }
-
-    private Recipe[] getRecipes() {
-        return new Recipe[] {
-                new Recipe(
-                        R.drawable.egg_in_bread,
-                        getString(R.string.eggs_in_bread),
-                        getString(R.string.eggs_in_bread_ingredients_line),
-                        getString(R.string.eggs_in_bread_ingredients),
-                        getString(R.string.eggs_in_bread_details)
-                ),
-                new Recipe(
-                        R.drawable.french_eggs,
-                        getString(R.string.french_eggs),
-                        getString(R.string.french_eggs_ingredients_line),
-                        getString(R.string.french_eggs_ingredients),
-                        getString(R.string.french_eggs_details)
-                ),
-                new Recipe(
-                        R.drawable.eggs_in_tomatoes,
-                        getString(R.string.eggs_in_tomatoes),
-                        getString(R.string.eggs_in_tomatoes_ingredients_line),
-                        getString(R.string.eggs_in_tomatoes_ingredients),
-                        getString(R.string.eggs_in_tomatoes_details)
-                ),
-                new Recipe(
-                        R.drawable.eggs_in_baskets,
-                        getString(R.string.eggs_in_baskets),
-                        getString(R.string.eggs_in_baskets_ingredients_line),
-                        getString(R.string.eggs_in_baskets_ingredients),
-                        getString(R.string.eggs_in_baskets_details)
-                ),
-                new Recipe(
-                        R.drawable.eggs_by_author,
-                        getString(R.string.eggs_by_author),
-                        getString(R.string.eggs_by_author_ingredients_line),
-                        getString(R.string.eggs_by_author_ingredients),
-                        getString(R.string.eggs_by_author_details)
-                )
-        };
     }
 
 }
