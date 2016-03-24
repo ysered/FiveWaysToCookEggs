c5e80cc279ea74e187934fc89cd9396db51394e2
Yuriy Seredyuk <yuriy.seredyuk@gmail.com>
2016-03-24 19:53:47 +0200

    Move getRecipes to data source object.

---
+++ app/src/main/java/com/example/android/fivewaystocookeggs/RecipeDataSource.java
@@ -0,0 +1,53 @@
+package com.example.android.fivewaystocookeggs;
+
+import android.content.Context;
+
+public class RecipeDataSource {
+
+    private final Context mContext;
+
+    public RecipeDataSource(Context context) {
+        mContext = context;
+    }
+
+    public Recipe[] getRecipes() {
+        return new Recipe[] {
+                new Recipe(
+                        R.drawable.egg_in_bread,
+                        mContext.getString(R.string.eggs_in_bread),
+                        mContext.getString(R.string.eggs_in_bread_ingredients_line),
+                        mContext.getString(R.string.eggs_in_bread_ingredients),
+                        mContext.getString(R.string.eggs_in_bread_details)
+                ),
+                new Recipe(
+                        R.drawable.french_eggs,
+                        mContext.getString(R.string.french_eggs),
+                        mContext.getString(R.string.french_eggs_ingredients_line),
+                        mContext.getString(R.string.french_eggs_ingredients),
+                        mContext.getString(R.string.french_eggs_details)
+                ),
+                new Recipe(
+                        R.drawable.eggs_in_tomatoes,
+                        mContext.getString(R.string.eggs_in_tomatoes),
+                        mContext.getString(R.string.eggs_in_tomatoes_ingredients_line),
+                        mContext.getString(R.string.eggs_in_tomatoes_ingredients),
+                        mContext.getString(R.string.eggs_in_tomatoes_details)
+                ),
+                new Recipe(
+                        R.drawable.eggs_in_baskets,
+                        mContext.getString(R.string.eggs_in_baskets),
+                        mContext.getString(R.string.eggs_in_baskets_ingredients_line),
+                        mContext.getString(R.string.eggs_in_baskets_ingredients),
+                        mContext.getString(R.string.eggs_in_baskets_details)
+                ),
+                new Recipe(
+                        R.drawable.eggs_by_author,
+                        mContext.getString(R.string.eggs_by_author),
+                        mContext.getString(R.string.eggs_by_author_ingredients_line),
+                        mContext.getString(R.string.eggs_by_author_ingredients),
+                        mContext.getString(R.string.eggs_by_author_details)
+                )
+        };
+    }
+
+}
